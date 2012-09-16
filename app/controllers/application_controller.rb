ecf51378d623c929d06df6928e37cef3920a3f8d
Rahul Shrivastava <rahul.shrivastava@gmail.com>
2012-09-15 21:54:10 -0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  # this is a new controller.
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '520348539e835a5c927d199ef9397b2d1d1c1bf596464e813818ee7be946e0cf9829f4c4ee200d42801a71349f717ecb218c99fd8eb8eaf896ff4b3d50238c7f'
